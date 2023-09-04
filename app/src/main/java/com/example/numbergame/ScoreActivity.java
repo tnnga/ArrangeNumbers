1816dec0dbd00c7ceaa7b028365a71b62ad7480b
nnga <nnga3363@gmail.com>
2023-09-04 21:20:29 +0700

    add sence

---
+++ app/src/main/java/com/example/numbergame/ScoreActivity.java
@@ -0,0 +1,13 @@
+package com.example.numbergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
